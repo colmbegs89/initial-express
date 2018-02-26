cee3372bc7f2bcaf06315244b83cd263befc0d35
Colm <colm@colms-mbp-3.derrycitycouncil.org>
2018-02-26 09:43:36 +0000

    post example

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,25 @@ let mileage = ''
       });
       // console.log(res.body.rows)
   })
-
+router.get('/getMileage', function(req, res){ 
+let mileage = ''
+  request('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=&destinations=New+York+City,NY&key=AIzaSyBfsDywzPknQNZLSXZukknAsrkNhaxlbnI', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred and handle it
+        console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+        console.log('body:',body); // Print the response status code if a response was received
+        console.log(JSON.parse(body).rows[0].elements[0].distance.text)
+        mileage = JSON.parse(body).rows[0].elements[0].distance.text
+        res.send(mileage)
+        // res.render('api', { title: mileage });
+      });
+      // console.log(res.body.rows)
+  })
+  router.post('/login',function(req,res){
+    var user_name=req.body.user;
+    var password=req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    res.end(JSON.stringify(req.body));
+  });
 // router.post('/create', (req, res) => {
 //   var options = {
 //     uri: process.env.SYS_API_URL + '/hardware/create',
